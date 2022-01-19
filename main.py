be0175c2e99783e0fbc44ae204f891f4d3f6d2b3
ColeFleming <47375701+derpferpmerp@users.noreply.github.com>
2022-01-19 19:54:03 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,101 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from utils import hidePlotBounds, constructBounds
+from tqdm import tqdm
+from colour import Color
+import cmath
+import math
+from matplotlib import colors
+
+
+def log2(x):
+	return cmath.log(x, 2)
+
+def sqrt(x):
+	return cmath.sqrt(x)
+
+def ComplexModulo(a,b):
+	x = a/b
+	x = round(x.real) + (round(x.imag)*1j)
+	z = x*b
+	return a-z
+
+C_RANGE = ((-2, 2), (-2, 2), 10) # ( x ) ( y ) ( num )
+ITERATIONS = 100 # Checking than z_n doesn't diverge
+fig, ax = plt.subplots(1, 1, figsize=(20,20))
+
+L_POINTS_X = np.linspace(*C_RANGE[0], C_RANGE[2])
+L_POINTS_Y = np.linspace(*C_RANGE[1], C_RANGE[2])
+POINTS_GRID = np.meshgrid(L_POINTS_X, L_POINTS_Y)
+POINTS = []
+for i in range(len(POINTS_GRID[0])):
+	PX, PY = POINTS_GRID[0][i], POINTS_GRID[1][i]
+	POINTS += list(zip(PX, PY))
+
+POINTS = [complex(x, y) for x, y in POINTS]
+
+def generateGradient(from_I, to_I, numPoints=2048):
+	cFrom = Color(rgb=from_I)
+	cTo = Color(rgb=to_I)
+	gradient = list(cFrom.range_to(cTo, numPoints))
+	grad_rgb = [x.rgb for x in gradient]
+	grad_rgb_255 = [list(map(lambda x: int(x*255), i)) for i in grad_rgb]
+	palette = np.array(grad_rgb_255)
+	return palette
+
+DEFAULT_COLOR = generateGradient((0, 0, 0), (0, 1, 0))
+
+def determineColor(nDiverge, i, scale=256, clr=DEFAULT_COLOR):
+	smoothed = log2(log2(nDiverge**2)/2)
+	colors = clr
+	colorI = (ComplexModulo(
+		math.floor((sqrt(i + 10 - smoothed) * 256).real),
+		len(colors)
+	)).real
+	colorI = math.floor(colorI)
+	return colors[colorI], colorI
+
+def generateCmap(boundMap):
+	# BoundMap = [ (color, number), ... ]
+	dedup = [boundMap[i] for i in range(len(boundMap)) if i == 0 or boundMap[i] != boundMap[i-1]]
+	boundMap = dedup
+	BOUNDS_L, COLOR_L = constructBounds(boundMap)
+	print(BOUNDS_L)
+	cmap = colors.ListedColormap(COLOR_L)
+	norm = colors.BoundaryNorm(BOUNDS_L, cmap.N)
+	return cmap, norm
+
+def isBounded(c, iterations=ITERATIONS):
+	bounded = True
+	z_n = 0
+	plotColor = [Color(rgb=(0,0,0)).rgb, 0]
+	while bounded and iterations > 0:
+		z_n1 = z_n*z_n + c
+		if abs(z_n1) > 2:
+			bounded=False
+			plotColor = determineColor(z_n1, ITERATIONS - iterations)
+		else:
+			z_n = z_n1
+		iterations -= 1
+	return plotColor
+
+
+GRID = np.zeros((C_RANGE[2], C_RANGE[2]), dtype=np.float64)
+cpoint = 0
+
+colorsL = []
+with tqdm(total=C_RANGE[2]**2) as pbar:
+	for x_i in range(C_RANGE[2]):
+		for y_i in range(C_RANGE[2]):
+			colorR, indR = isBounded(POINTS[cpoint])
+			GRID[x_i, y_i] = indR
+			cpoint += 1
+			colorsL.append((colorR, indR))
+			pbar.update(1)
+
+cmap, norm = generateCmap(colorsL)
+
+
+plt.imshow(GRID, cmap=cmap, norm=norm)
+hidePlotBounds(ax)
+plt.savefig("out.png")
---
+++ utils.py
@@ -0,0 +1,38 @@
+import numpy as np
+
+
+def hidePlotBounds(ax):
+	ax.spines['top'].set_visible(False)
+	ax.spines['right'].set_visible(False)
+	ax.spines['bottom'].set_visible(False)
+	ax.spines['left'].set_visible(False)
+
+	ax.set_xticklabels([])
+	ax.set_yticklabels([])
+
+	ax.set_xticks([])
+	ax.set_yticks([])
+
+	ax.axes.get_xaxis().set_visible(False)
+	ax.axes.get_yaxis().set_visible(False)
+
+def constructBounds(lst, tau=0.1):
+	# LST : [ ( ( R, G, B ), INDX ) ... ]
+	# Tau : Mult of Distance for Spacing, D*(1-tau) < D < D*(1+tau) 
+	lstSorted = sorted(lst, key=lambda x: x[1])
+	lstSorted = [ (x[0], x[1] * np.array([1-tau, 1+tau])) for x in lstSorted]
+	FINALBOUNDS = []
+	FINALCOLORS = []
+	for i in range(len(lstSorted)):
+		choice = "L" if (i == len(lstSorted) - 1) else ("F" if i==0 else "M")
+		item = lstSorted[i]
+		if choice == "F":
+			FINALBOUNDS.append(0)
+		elif choice == "L":
+			FINALBOUNDS.append(item[1][0])
+			FINALBOUNDS.append(item[1][-1])
+		elif choice == "M":
+			FINALBOUNDS.append(item[1][0])
+		FINALCOLORS.append(item[0])
+			
+	return FINALBOUNDS, FINALCOLORS
